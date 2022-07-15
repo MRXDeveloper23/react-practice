4d9156ba1d9ba3adfef6c8defeac4b47819cca9a
Khurmatillo <72973599+MRXDeveloper23@users.noreply.github.com>
2022-07-15 17:46:10 +0500

    feat: initial setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (name, age, email) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name, age, email }];
+    });
+  };
+  return (
+    <div className="App">
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
